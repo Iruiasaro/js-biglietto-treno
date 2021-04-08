7ea3f7c5467f6b18784789ce9e9662e687782697
Iruiasaro <brunettirosario@gmail.com>
2021-04-08 16:00:43 +0200

    prima impostazione esercizio

---
+++ script.js
@@ -0,0 +1,5 @@
+var km = parseInt(prompt ("Quanti Km dovrà percorrere?"));
+document.getElementById("km").innerHTML = km;
+
+var age = parseInt(prompt ("Quanti anni ha?"));
+document.getElementById("age").innerHTML = age;
