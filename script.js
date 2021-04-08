d326b8d62d78739707964941094c05114aa515e5
Iruiasaro <brunettirosario@gmail.com>
2021-04-08 16:43:57 +0200

    fornisce l'output richiesto

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+var kmEuro = 0.21;
+var prezzoFinale = 0;
+
 var km = parseInt(prompt ("Quanti Km dovrà percorrere?"));
-document.getElementById("km").innerHTML = km;
+document.getElementById("chilometri").innerHTML = km;
 
 var age = parseInt(prompt ("Quanti anni ha?"));
-document.getElementById("age").innerHTML = age;
+document.getElementById("età").innerHTML = age;
+
+if (age < 18) {
+  kmEuro -= kmEuro * 0.2;
+  prezzoFinale = km * kmEuro;
+  text_msg = "Il costo finale del biglietto è di €" + prezzoFinale.toFixed(2) + ", è stato applicato lo sconto minorenni del 20%!";
+} else if (age > 65) {
+  kmEuro -= kmEuro * 0.4;
+  prezzoFinale = km * kmEuro;
+  text_msg = "Il costo finale del biglietto è di €" + prezzoFinale.toFixed(2) + ", è stato applicato lo sconto over 65 del 40%!";
+} else {
+  prezzoFinale = km * kmEuro;
+  text_msg = "Il costo finale del biglietto è di €" + prezzoFinale + "!";
+}
+
+document.getElementById("risultato").innerHTML = text_msg;
